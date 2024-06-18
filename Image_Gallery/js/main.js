0b296d6c9289326ed0c2389575469b8eda0bc3b9
Muhammed Shibil M <95218558+muhammedshibilm@users.noreply.github.com>
2024-06-18 20:57:57 +0530

    Final commit

--- Image_Gallery/js/main.js
+++ Image_Gallery/js/main.js
@@ -1,7 +1,19 @@
+//overlay container 
 var overlayColor = document.querySelector('.overlay')
+// Color changing button 
 var button=document.querySelector('.dark')
-
-let values=true
+// images names
+const imagesArray=[
+  {"url":"Images/pic1.jpg","alt":"human blue eye"},
+  {"url":"Images/pic2.jpg","alt":"White smokes"},
+  {"url":"Images/pic3.jpg","alt":"Blue flowers"},
+  {"url":"Images/pic4.jpg","alt":"Pyramid inside pic"},
+  {"url":"Images/pic5.jpg","alt":"Butterfly"},
+]
+//Images list displaying container 
+var smallScreen=document.querySelector('.thumb-bar')
+//Big Screen image tag 
+var bigScreen=document.querySelector('.displayed-img')
 //changing overlay div and button text 
 function overlayButton(){
     //Get current class name of that button
@@ -20,3 +32,30 @@ function overlayButton(){
 
 // Button event hander 
 button.addEventListener('click',()=>overlayButton());
+
+//thumb-bar images display
+document.addEventListener('DOMContentLoaded',()=>{
+    
+    //display image function
+    function displayImages(url){
+       
+        for (let index = 0; index < url.length; index++) {
+            //small screen image tag
+            let img = document.createElement('img');
+            //Geting each images from array 
+            let images=url[index];
+            img.src=images.url;
+            img.alt=images.alt;
+            img.addEventListener('click',()=>showBigImage(images.url,images.alt))
+            //aading the images in the thumb-bar container
+            smallScreen.appendChild(img)
+        }
+    }
+    displayImages(imagesArray);
+})
+
+function showBigImage(url,alt) {
+    console.log(url,alt);
+    bigScreen.src=url;
+    bigScreen.alt=alt;
+}
