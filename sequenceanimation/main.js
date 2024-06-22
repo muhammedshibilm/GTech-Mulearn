fec56f4a852e1c76bcf6ce726db37f6820fd067b
Muhammed Shibil M <95218558+muhammedshibilm@users.noreply.github.com>
2024-06-22 20:17:57 +0530

    Final commit

--- sequenceanimation/main.js
+++ sequenceanimation/main.js
@@ -1,8 +1,9 @@
+//Declare the variables 
 const item1=document.querySelector('#itemone');
 const item2=document.querySelector('#itemtwo');
 const item3=document.querySelector('#itemthree');
 
-
+//animation doing function
 function animation(items,duration) {
     items.animate([
         { transform: 'rotate(0deg) scale(1)' },
@@ -14,11 +15,22 @@ function animation(items,duration) {
     });
 }
 
+//animation excution function
 function startAnimation() {
     const duration=2000;
     
-    
+    //first animation
     animation(item1,duration)
+
+    //second animation
+    setTimeout(() => {
+        animation(item2,duration)
+    }, duration);
+
+    //Third animation
+    setTimeout(() => {
+        animation(item3,duration)
+    }, duration*2);
 
 }
 
